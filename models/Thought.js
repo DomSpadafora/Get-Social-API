79b462a5ad05f7857cd82115080998e206c84f40
DomSpadafora <120040191+DomSpadafora@users.noreply.github.com>
2023-02-22 15:41:27 -0500

    added model for thought

--- models/Thought.js
+++ models/Thought.js
@@ -1,29 +1,27 @@
 const { Schema, model } = require('mongoose');
 
-const courseSchema = new Schema(
+//need to create Reaction (Schema Only)
+
+const thoughtSchema = new Schema(
   {
-    courseName: {
+    thoughtText: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      //use a getter method to format the timestamp on query
+    },
+    username: {
       type: String,
       required: true,
     },
-    inPerson: {
-      type: Boolean,
-      default: true,
+    reaction: {
+      //array of nested documents created with the reactionSchema
     },
-    startDate: {
-      type: Date,
-      default: Date.now(),
-    },
-    endDate: {
-      type: Date,
-      default: () => new Date(+new Date() + 84 * 24 * 60 * 60 * 1000),
-    },
-    students: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: 'Student',
-      },
-    ],
+
   },
   {
     toJSON: {
@@ -33,6 +31,6 @@ const courseSchema = new Schema(
   }
 );
 
-const Course = model('course', courseSchema);
+const Thought = model('Thought', thoughtSchema);
 
 module.exports = Thought;
